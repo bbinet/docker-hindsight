bd7e18b74296b21fc7d17af904904b1a0c3a7e39
Bruno Binet <bruno.binet@gmail.com>
2018-06-05 14:58:41 +0200

    Update lsb/lsbext/hindsight version to latest
    and add support for filtering some pkg to include/exclude when installing

--- install_debs.sh
+++ install_debs.sh
@@ -4,12 +4,17 @@ set -e
 
 base_url="$1"
 md5sum="$2"
+filter="$3"
 
 mkdir debs
 (cd debs;
     wget "${base_url}/md5sums"
     echo "${md5sum}  md5sums" | md5sum --check
-    cat md5sums | while read line
+    md5sums=$(cat md5sums)
+    if ! [ -z "$filter" ]; then
+        md5sums=$(cat md5sums | $filter)
+    fi
+    echo "$md5sums" | while read line
 do
         pkg="${line#*  }"
         wget --retry-connrefused "${base_url}/${pkg}"
